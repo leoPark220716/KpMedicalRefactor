5a115a2461e39fa580d7f0370996579acfb32ca9
leoPark220716 <109401877+leoPark220716@users.noreply.github.com>
2024-11-01 16:17:48 +0900

    Delete Pods directory

--- Pods/Target Support Files/Pods-KpMedicalWallet-KpMedicalWalletUITests/Pods-KpMedicalWallet-KpMedicalWalletUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_KpMedicalWallet_KpMedicalWalletUITests : NSObject
-@end
-@implementation PodsDummy_Pods_KpMedicalWallet_KpMedicalWalletUITests
-@end
--- Pods/Target Support Files/Pods-KpMedicalWalletTests/Pods-KpMedicalWalletTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_KpMedicalWalletTests : NSObject
-@end
-@implementation PodsDummy_Pods_KpMedicalWalletTests
-@end
